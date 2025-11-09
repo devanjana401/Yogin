f9c814e5c15a1ece85bcf861d5f5b2ffd9636c30
devanjana401 <anjanasasiaduvad@gmail.com>
2025-11-09 16:20:54 +0530

    Added new files

---
+++ assets/js/json.js
@@ -0,0 +1,21 @@
+async function loadGallery() {
+      try {
+        const response = await fetch('./gallery.json'); 
+        const data = await response.json();
+        const galleryRow = document.querySelector('.row.g-2');
+
+        data.gallery.forEach(item => {
+          const col = document.createElement('div');
+          col.className = 'col-6 col-md-3';
+          col.innerHTML = `
+            <div class="card h-100 border-0 text-white position-relative">
+              <img src="${item.src}" class="card-img" alt="${item.alt}">
+            </div>`;
+          galleryRow.appendChild(col);
+        });
+      } catch (error) {
+        console.error('Error loading gallery:', error);
+      }
+    }
+
+    loadGallery();
